17904f7621df8ba8a41a8d3db60b7700be4a2313
bunkerity <contact@bunkerity.com>
2022-06-27 14:00:43 +0200

    slack - init work

---
+++ slack/README.md
@@ -0,0 +1,92 @@
+# Slack plugin
+
+<p align="center">
+	<img alt="BunkerWeb Slack diagram" src="https://github.com/bunkerity/bunkerweb-plugins/raw/main/slack/docs/diagram.svg" />
+</p>
+
+This [BunkerWeb](https://www.bunkerweb.io) plugin will automatically send you attack notifications on a Slack channel of your choice using a webhook.
+
+# Table of contents
+
+- [Slack plugin](#slack-plugin)
+- [Table of contents](#table-of-contents)
+- [Prerequisites](#prerequisites)
+- [Setup](#setup)
+- [Settings](#settings)
+
+# Prerequisites
+
+Please read the [plugins section](https://docs.bunkerweb.io/plugins) of the BunkerWeb documentation first.
+
+You will need to setup a Slack webhook URL, you will find more information [here](https://api.slack.com/messaging/webhooks).
+
+# Setup
+
+See the [plugins section](https://docs.bunkerweb.io/plugins) of the BunkerWeb documentation for the installation procedure depending on your integration.
+
+There is no additional services to setup besides the plugin itself.
+
+## Docker
+
+```yaml
+version: '3'
+
+services:
+
+  bunkerweb:
+    image: bunkerity/bunkerweb:1.4.1
+    ...
+    environment:
+      - USE_SLACK=yes
+      - SLACK_WEBHOOK_URL=https://api.slack.com/messaging/webhooks/...
+    ...
+```
+
+## Swarm
+
+```yaml
+version: '3.5'
+
+services:
+
+  mybunker:
+    image: bunkerity/bunkerweb:1.4.1
+    ...
+    environment:
+      - USE_SLACK=yes
+      - SLACK_WEBHOOK_URL=https://api.slack.com/messaging/webhooks/...
+    ...
+```
+
+## Kubernetes
+
+```yaml
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: ingress
+  annotations:
+    bunkerweb.io/USE_SLACK: "yes"
+    bunkerweb.io/SLACK_WEBHOOK_URL: "https://api.slack.com/messaging/webhooks/..."
+...
+```
+
+# Settings
+
+| Setting | Default | Description |
+| :-----: | :-----: | :---------- |
+| `USE_SLACK` | `no` | When set to `yes`, notifications of denied requests will be sent to a Slack webhook. |
+| `SLACK_WEBHOOK_URL` | `https://api.slack.com/messaging/webhooks/...` | Address of the Slack webhook where notifications will be sent to. |
+| `SLACK_RETRY_IF_LIMITED` | `no` | Slack is applying a rate-limit ton their API. When this settings is set to `yes`, the plugin will retry to send the notification later. It may consumes some resources if you are under heavy attacks by the way. |
+
+# TODO
+
+* Add more info in notification :
+  * Date
+  * Country of IP
+  * ASN of IP
+  * ...
+* Add settings to control what details to send :
+  * Anonymize IP
+  * Add body
+  * Add headers
--- slack/slack.lua
+++ slack/slack.lua
@@ -8,30 +8,30 @@ local http		= require "resty.http"
 
 function _M.new()
 	local self = setmetatable({}, _M)
-	local value, err = utils.get_variable("DISCORD_WEBHOOK_URL", false)
+	local value, err = utils.get_variable("SLACK_WEBHOOK_URL", false)
 	if not value then
-		logger.log(ngx.ERR, "DISCORD", "error while getting DISCORD_WEBHOOK_URL setting : " .. err)
-		return nil, "error while getting DISCORD_WEBHOOK_URL setting : " .. err
+		logger.log(ngx.ERR, "SLACK", "error while getting SLACK_WEBHOOK_URL setting : " .. err)
+		return nil, "error while getting SLACK_WEBHOOK_URL setting : " .. err
 	end
 	self.webhook = value
-	local value, err = utils.get_variable("DISCORD_RETRY_IF_LIMITED", false)
+	local value, err = utils.get_variable("SLACK_RETRY_IF_LIMITED", false)
 	if not value then
-		logger.log(ngx.ERR, "DISCORD", "error while getting DISCORD_RETRY_IF_LIMITED setting : " .. err)
-		return nil, "error while getting DISCORD_RETRY_IF_LIMITED setting : " .. err
+		logger.log(ngx.ERR, "SLACK", "error while getting SLACK_RETRY_IF_LIMITED setting : " .. err)
+		return nil, "error while getting SLACK_RETRY_IF_LIMITED setting : " .. err
 	end
 	self.retry = value
 	return self, nil
 end
 
-function _M:log(bypass_use_discord)
-	if not bypass_use_discord then
-		-- Check if discord is activated
-		local check, err = utils.get_variable("USE_DISCORD")
+function _M:log(bypass_use_slack)
+	if not bypass_use_slack then
+		-- Check if Slack is activated
+		local check, err = utils.get_variable("USE_SLACK")
 		if check == nil then
-			return false, "error while getting variable USE_DISCORD (" .. err .. ")"
+			return false, "error while getting variable USE_SLACK (" .. err .. ")"
 		end
 		if check ~= "yes" then
-			return true, "Discord plugin not enabled"
+			return true, "Slack plugin not enabled"
 		end
 	end
 
@@ -45,7 +45,7 @@ function _M:log(bypass_use_discord)
 	local function send(premature, obj, data)
 		local httpc, err = http.new()
 		if not httpc then
-			logger.log(ngx.ERR, "DISCORD", "can't instantiate http object : " .. err)
+			logger.log(ngx.ERR, "SLACK", "can't instantiate http object : " .. err)
 		end
 		local res, err_http = httpc:request_uri(obj.webhook, {
 			method = "POST",
@@ -57,34 +57,34 @@ function _M:log(bypass_use_discord)
 		})	
 		httpc:close()
 		if not res then
-			logger.log(ngx.ERR, "DISCORD", "error while sending request : " .. err)
+			logger.log(ngx.ERR, "SLACK", "error while sending request : " .. err)
 		end
 		if obj.retry == "yes" and res.status == 429 and res.headers["Retry-After"] then
-			logger.log(ngx.WARN, "DISCORD", "Discord API is rate-limiting us, retrying in " .. res.headers["Retry-After"] .. "s")
+			logger.log(ngx.WARN, "SLACK", "Slack API is rate-limiting us, retrying in " .. res.headers["Retry-After"] .. "s")
 			local hdr, err = ngx.timer.at(res.headers["Retry-After"], send, obj, data)
 			if not hdr then
-				logger.log(ngx.ERR, "DISCORD", "can't create report timer : " .. err)
+				logger.log(ngx.ERR, "SLACK", "can't create report timer : " .. err)
 				return
 			end
 			return
 		end
 		if res.status < 200 or res.status > 299 then
-			logger.log(ngx.ERR, "DISCORD", "request returned status " .. tostring(res.status))
+			logger.log(ngx.ERR, "SLACK", "request returned status " .. tostring(res.status))
 			return
 		end
-		logger.log(ngx.INFO, "DISCORD", "request sent to webhook")
+		logger.log(ngx.INFO, "SLACK", "request sent to webhook")
 	end
 	local data = {}
-	data.content = "```Denied request for IP " .. ngx.var.remote_addr .. " (reason = " .. reason .. ").\n\nRequest data :\n\n" .. ngx.var.request .. "\n"
+	data.text = "```Denied request for IP " .. ngx.var.remote_addr .. " (reason = " .. reason .. ").\n\nRequest data :\n\n" .. ngx.var.request .. "\n"
 	local headers, err = ngx.req.get_headers()
 	if not headers then
-		data.content = data.content .. "error while getting headers : " .. err
+		data.text = data.text .. "error while getting headers : " .. err
 	else
 		for header, value in pairs(headers) do
-			data.content = data.content .. header .. ": " .. value .. "\n"
+			data.text = data.text .. header .. ": " .. value .. "\n"
 		end
 	end
-	data.content = data.content .. "```"
+	data.text = data.text .. "```"
 	local hdr, err = ngx.timer.at(0, send, self, data)
 	if not hdr then
 		return false, "can't create report timer : " .. err
@@ -94,13 +94,13 @@ function _M:log(bypass_use_discord)
 end
 
 function _M:log_default()
-	-- Check if discord is activated
-	local check, err = utils.has_variable("USE_DISCORD", "yes")
+	-- Check if SLACK is activated
+	local check, err = utils.has_variable("USE_SLACK", "yes")
 	if check == nil then
-		return false, "error while checking variable USE_DISCORD (" .. err .. ")"
+		return false, "error while checking variable USE_SLACK (" .. err .. ")"
 	end
 	if not check then
-		return true, "Discord plugin not enabled"
+		return true, "Slack plugin not enabled"
 	end
 	-- Check if default server is disabled
 	local check, err = utils.get_variable("DISABLE_DEFAULT_SERVER", false)
