907efa972305014740b3720b8590e60e6e33d2fd
florian <contact@bunkerity.com>
2023-05-22 20:24:57 +0200

    tests - fix directory when build bw (again)

--- .tests/bw.sh
+++ .tests/bw.sh
@@ -3,15 +3,17 @@
 . ./.tests/utils.sh
 
 echo "ℹ️ Pulling repo ..."
+cd /tmp
 do_and_check_cmd git clone https://github.com/bunkerity/bunkerweb.git
 
 echo "ℹ️ Checkout branch ..."
-CHANGE_DIR=bunkerweb do_and_check_cmd git checkout $1
+cd bunkerweb
+do_and_check_cmd git checkout $1
 
 echo "ℹ️ Build BW ..."
-CHANGE_DIR=bunkerweb do_and_check_cmd docker build -t bunkerweb:tests -f src/bw/Dockerfile .
+do_and_check_cmd docker build -t bunkerweb:tests -f src/bw/Dockerfile .
 
 echo "ℹ️ Build scheduler ..."
-CHANGE_DIR=bunkerweb do_and_check_cmd docker build -t bw-scheduler:tests -f src/scheduler/Dockerfile .
+do_and_check_cmd docker build -t bw-scheduler:tests -f src/scheduler/Dockerfile .
 
 echo "ℹ️ Images built"
