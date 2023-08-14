85244ba270428108fb2860c7a694ba5315dbdf4f
florian <contact@bunkerity.com>
2023-08-14 16:19:19 +0200

    use existing images in tests and temp remove select type in crowdsec settings

--- .tests/build-push.sh
+++ .tests/build-push.sh
@@ -3,7 +3,11 @@
 . .tests/utils.sh
 
 echo "ℹ️ Build bunkerweb-coraza ..."
-CHANGE_DIR="./coraza/api" do_and_check_cmd docker build -t bunkerity/bunkerweb-coraza:latest .
+CHANGE_DIR="./coraza/api" do_and_check_cmd docker build -t bunkerweb-coraza .
+
+echo "ℹ️ Tag bunkerweb-coraza ..."
+do_and_check_cmd docker image tag bunkerweb-coraza bunkerity/bunkerweb-coraza:latest
+do_and_check_cmd docker image tag bunkerweb-coraza "bunkerity/bunkerweb-coraza:$1"
 
 echo "ℹ️ Push bunkerweb-coraza ..."
-do_and_check_cmd docker push bunkerity/bunkerweb-coraza:latest
+do_and_check_cmd docker image push --all-tags bunkerity/bunkerweb-coraza
